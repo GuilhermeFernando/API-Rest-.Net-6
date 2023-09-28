544c7b53bb4aaac79457fbadb1e851b9227ccb32
Guilherme Machado <guifermach@hotmail.com>
2023-09-28 11:04:49 -0300

    Implementando status code para cada endpoint

--- Program.cs
+++ Program.cs
@@ -6,21 +6,29 @@ var app = builder.Build();
 
 app.MapPost("/produto", (Produto produto) => {
    RepositorioProduto.Add(produto);
+   return Results.Created($"/produto/{produto.Codigo}",produto.Codigo);
+    
 });
 
 app.MapGet("/produto/{code}",([FromRoute] string code) => {
     var produto = RepositorioProduto.GetBy(code); 
-    return produto;
+   if(produto != null)
+   {
+    return Results.Ok(produto);
+   }
+   return Results.NotFound();
 });
 
 app.MapPut("/produto",(Produto produto) => {
     var produtoSave = RepositorioProduto.GetBy(produto.Codigo);
     produtoSave.Nome = produto.Nome;
+    return Results.Ok();
 });
 
 app.MapDelete("/produto/{code}",([FromRoute] String code  ) => {
     var produtoSave = RepositorioProduto.GetBy(code);
     RepositorioProduto.Remove(produtoSave);
+    return Results.Ok();
 });
 
 app.Run();
